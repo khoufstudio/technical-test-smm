6d5f30dabd7b5313ea82ac809efc22e19cfef72c
khoufstudio <rizqy.nurhaqy@gmail.com>
2021-11-26 22:57:02 +0700

    add fake and seeder for Customer

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Faker\Provider\fr_FR\Address;
+
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $this->faker->addProvider(new Address($this->faker));
+        return [
+            'name' => $this->faker->name(),
+            'departement' => $this->faker->departmentName(),
+            //'departement' => $this->faker->name(),
+            'nik' => $this->faker->nik()            
+            //'nik' => $this->faker->numerify('################')
+        ];
+    }
+}
---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Customer;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // create 10 random data from CustomerFactory
+        Customer::factory()->count(10)->create();
+    }
+}
