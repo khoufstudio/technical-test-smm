5c1a2ffdcacb81bae297760ef7646c9b736cc529
khoufstudio <rizqy.nurhaqy@gmail.com>
2021-11-28 05:35:54 +0700

    change title page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" value="{{ csrf_token() }}" />
-    <title>VueJS CRUD Operations in Laravel</title>
+    <title>Techincal Test PT SMM</title>
     <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
 </head>
 
