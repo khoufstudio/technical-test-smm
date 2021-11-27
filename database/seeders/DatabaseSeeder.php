96be95a5fbd4b89a0149a4be90c1b3e7ca9a2d2d
khoufstudio <rizqy.nurhaqy@gmail.com>
2021-11-27 13:35:59 +0700

    adjustment seeder configuration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            CustomerSeeder::class,
+            ProductSeeder::class,
+            ProductRequestSeeder::class,
+            ProductRequestListSeeder::class,
+        ]);
     }
 }
